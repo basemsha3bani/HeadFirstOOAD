04f2bab9be8573bc915497eaf45a90eebeae4f8d
basem <basem_sha3bani@hotmaill.com>
2025-10-18 17:37:01 +0300

    test auto deploy

--- HeadFirstOOAD/Controllers/WeatherForecastController.cs
+++ HeadFirstOOAD/Controllers/WeatherForecastController.cs
@@ -27,13 +27,13 @@ namespace HeadFirstOOAD.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            //return Enumerable.Range(1, 4).Select(index => new WeatherForecast
+            //{
+            //    Date = DateTime.Now.AddDays(index),
+            //    TemperatureC = rng.Next(-10, 55),
+            //    Summary = Summaries[rng.Next(Summaries.Length)]
+            //})
+           return new List<WeatherForecast>() .ToArray();
         }
     }
 }
